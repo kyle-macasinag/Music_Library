b5a2eee90731625870289b20b6affd26e0b8c177
kyle.macasinag <kylenmac@verizon.net>
2022-05-05 16:51:01 -0400

    Plugged serializer into first get function

--- music_library/views.py
+++ music_library/views.py
@@ -1,9 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .models import Music
+from .serializers import MusicSerializer
 
 @api_view(["GET"])
 def music_list(request):
+    music = Music.objects.all
+    serializer = MusicSerializer(music, many=True)
 
-
-    return Response("ok")
+    return Response(serializer.data)
